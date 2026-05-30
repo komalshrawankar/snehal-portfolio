43abbc126d3b7ffa954b19aaefcaf856f207319a
komal shrawankar <shrawankarkomal9@gmail.com>
2026-05-30 12:11:28 +0530

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SnehalPortfolio from "./pages/snehalportfolio"
+
+const App = () => {
+  return (
+   <>
+   <SnehalPortfolio/>
+   </>
+  )
+}
+
+export default App
